54a775d1203aacd77b74011b6f446c28b49048a9
Calum <calumcmccall@gmail.com>
2022-09-07 08:40:21 +0100

    library has a capacity

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> stock;
+    private int capacity;
 
-    public Library(){
+    public Library(int capacity){
         this.stock = new ArrayList<Book>();
+        this.capacity = capacity;
     }
 
     public int getStockCount(){
@@ -13,6 +15,13 @@ public class Library {
     }
 
     public void addBook(Book book){
-        stock.add(book);
+        boolean libraryStockFull = isStockFull();
+        if(libraryStockFull == false) {
+            stock.add(book);
+        }
+    }
+
+    public boolean isStockFull(){
+        return (this.stock.size() < this.capacity ? false : true);
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -8,12 +8,16 @@ public class LibraryTest {
     Library library;
     Book book1;
     Book book2;
+    Book book3;
+    Book book4;
 
     @Before
     public void setUp(){
-        library = new Library();
+        library = new Library(3);
         book1 = new Book("Baked Treats", "Mary Berry", "Cooking");
         book2 = new Book("Beautiful Code", "Calum McCall", "Computing");
+        book3 = new Book("More Beautiful Code", "Calum McCall", "Computing");
+        book4 = new Book("Ugly Code", "Calum McCall", "Computing");
         library.addBook(book1);
     }
 
@@ -26,6 +30,14 @@ public class LibraryTest {
     public void canAddBookToLibrary(){
         library.addBook(book2);
         assertEquals(2, library.getStockCount());
+    }
+
+    @Test
+    public void cantAddIfCapacityMet(){
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        assertEquals(3, library.getStockCount());
     }
 
 }
